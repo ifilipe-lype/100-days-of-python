a4ca04a3abe5f03b2c516c1f0d6352d5ea2e447f
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-07 10:20:14 +0100

    feat: adds initial snake's body

--- 20-snake-game/main.py
+++ 20-snake-game/main.py
@@ -2,9 +2,18 @@ from turtle import Screen, Turtle
 
 screen = Screen()
 
+# screen setup
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake_game")
+
+# initial snake's body
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    segment = Turtle("square")
+    segment.color("white")
+    segment.goto(position)
 
 
 screen.exitonclick()
