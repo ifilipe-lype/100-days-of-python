dc583ef03d4a08fe97c59722bf6bab0d494fa8a6
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-07 10:58:52 +0100

    feat: refactors snake code into it's on class

--- 20-snake-game/main.py
+++ 20-snake-game/main.py
@@ -1,40 +1,23 @@
-from turtle import Screen, Turtle
 import time
+from turtle import Screen, Turtle
+from snake import Snake
 
-screen = Screen()
 
 # screen setup
+screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake_game")
 screen.tracer(0)
 
-# initial snake's body
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-segments = []
-
-for position in starting_position:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.penup()
-    segment.goto(position)
-
-    segments.append(segment)
-
+# Main game loop
 game_is_on = True
-
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
-    head_segment = segments[0]
-    head_segment.forward(20)
-
-    for seg_idx in range(len(segments) - 1, 0, -1):
-        segment = segments[seg_idx]
-        next_segment = segments[seg_idx - 1]
-
-        segment.goto(next_segment.position())
+    time.sleep(0.1)
+    snake.move()
 
 
 screen.exitonclick()
