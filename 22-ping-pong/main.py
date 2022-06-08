3b53ad64b619947ca27cf02e0b2e3de34009f108
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-08 12:00:15 +0100

    feat: adds paddle/bars to the screen with vertical moviment

--- 22-ping-pong/main.py
+++ 22-ping-pong/main.py
@@ -1,9 +1,27 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("pyng-pong")
+screen.tracer(0)
+
+right_paddle = Paddle(x_pos=350)
+left_paddle = Paddle(x_pos=-350)
+
+screen.listen()
+
+screen.onkey(right_paddle.move_up, "Up")
+screen.onkey(right_paddle.move_down, "Down")
+
+screen.onkey(left_paddle.move_up, "w")
+screen.onkey(left_paddle.move_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
---
+++ 22-ping-pong/paddle.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, x_pos):
+        super().__init__()
+
+        self.shape("square")
+        self.penup()
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+
+        self.goto(x_pos, 0)
+
+    def move_up(self):
+        new_y_pos = self.ycor() + 20
+        print(f"{new_y_pos=}")
+        if new_y_pos < 280:
+            self.goto(x=self.xcor(), y=new_y_pos)
+
+    def move_down(self):
+        new_y_pos = self.ycor() - 20
+        print(f"{new_y_pos=}")
+        if new_y_pos > -280:
+            self.goto(x=self.xcor(), y=new_y_pos)
