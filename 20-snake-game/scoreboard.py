4c6a72f7c9ec5b9d5d643a7199198589cf7a6038
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-12 20:42:51 +0100

    feat: adds highscore board

--- 20-snake-game/scoreboard.py
+++ 20-snake-game/scoreboard.py
@@ -10,6 +10,7 @@ class Scoreboard(Turtle):
         super().__init__()
 
         self.score = 0
+        self.high_score = 0
 
         self.color("white")
         self.penup()
@@ -20,10 +21,18 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} | HighScore: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+
+        self.score = 0
+
         self.update_scoreboard()
 
     def game_over(self):
