908923f74a64aa2a741cf8e8598938b11216777c
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-16 09:15:22 +0100

    feat: creates game's main loop logic

--- 25-US-States-game/main.py
+++ 25-US-States-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 US_IMG_PATH = "blank_states_img.gif"
 
@@ -10,9 +11,35 @@ screen.addshape(US_IMG_PATH)
 
 turtle.shape(US_IMG_PATH)
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state name ?");
+state_cors_data = pd.read_csv("50_states.csv")
 
-print(answer_state)
+# scores
+answered_states = []
+
+game_on = True
+
+while game_on and len(answered_states) <= 2:
+    answer_state = screen.textinput(title="Guess the state", prompt="What's another state name ?")
+
+    if answer_state is None:
+        game_on = False
+        break
+
+    answer_state = answer_state.title()
+
+    state = state_cors_data[state_cors_data.state == answer_state]
+
+    if not state.empty:
+        state_label = turtle.Turtle()
+        state_label.hideturtle()
+        state_label.penup()
+        state_label.goto(int(state.x), int(state.y))
+        state_label.write(answer_state)
+
+        screen.update()
+        answered_states.append(answer_state)
+    else:
+        pass
 
 
 screen.mainloop()
