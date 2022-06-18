4869d8d624992d1fb5f1fb1af37860c4fddf1b84
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-18 13:17:56 +0100

    feat: adds buttons and labels

--- 28-pomodoro-app/main.py
+++ 28-pomodoro-app/main.py
@@ -1,4 +1,6 @@
-from tkinter import PhotoImage, Tk, Canvas
+from cgitb import text
+from ctypes import alignment
+from tkinter import Button, PhotoImage, Tk, Canvas, Label
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -22,11 +24,25 @@ window = Tk()
 window.title("Pomodoro App")
 window.config(bg=YELLOW, padx=100, pady=50)
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_btn = Button(text="start", highlightthickness=0)
+start_btn.grid(column=0, row=2)
+
+reset_btn = Button(text="reset", highlightthickness=0)
+reset_btn.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 30))
+check_marks.grid(column=1, row=3)
+
+
 
 
 
