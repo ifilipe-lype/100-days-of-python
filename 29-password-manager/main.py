43ccd5f3a27b958b7618e0ab0dbd9aa89e2003e5
ifilipe-lype <ifilipe.lype@gmail.com>
2022-06-22 09:33:44 +0100

    feat: creates initial screen

--- 29-password-manager/main.py
+++ 29-password-manager/main.py
@@ -1,5 +1,29 @@
+from tkinter import Tk, Canvas, PhotoImage
+
+
+
+# CONSTANTS VALUES
+LOGO_IMG_PATH = 'logo.png'
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(bg="#FFF", padx=20, pady=20)
+window.minsize(width=540, height=620)
+
+
+canvas = Canvas(width=200, height=200)
+canvas.config(bg="#FFF", highlightthickness=0)
+
+logo_img = PhotoImage(file=LOGO_IMG_PATH)
+
+canvas.create_image(100, 100, image=logo_img)
+
+canvas.pack()
+
+window.mainloop()
